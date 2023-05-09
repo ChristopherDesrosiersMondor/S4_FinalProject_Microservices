a6608756d717ce874449fbc54120c93aaffd05fa
ChristopherDesrosiersMondor <desrosch@gmail.com>
2023-05-09 15:29:01 -0400

    trying consul configuration

--- ms_community/src/main/java/com/example/ms_community/controller/CommunityController.java
+++ ms_community/src/main/java/com/example/ms_community/controller/CommunityController.java
@@ -1,7 +1,5 @@
 package com.example.ms_community.controller;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -34,38 +32,31 @@ import com.example.ms_community.model.Community;;
 public class CommunityController {
     @Autowired
     private CommunityRepository communityRepository;
-    
+
     @Operation(summary = "Adds a community do the database")
-    @ApiResponses(value = { 
-        @ApiResponse(responseCode = "201", description = "Community created", 
-            content = { @Content(mediaType = "application/json", 
-            schema = @Schema(implementation = Community.class)) }),
-        @ApiResponse(responseCode = "400", description = "Invalid data", 
-            content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal server error", 
-            content = @Content)
-        })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Community created", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Community.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid data", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @PostMapping("/add")
     public ResponseEntity<Community> addNewCommunity(@RequestBody Community newCommunity) {
         try {
             Community communityToAdd = communityRepository.save(new Community(newCommunity));
             return new ResponseEntity<>(communityToAdd, HttpStatus.CREATED);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @Operation(summary = "Gets all communities")
-    @ApiResponses(value = { 
-        @ApiResponse(responseCode = "200", description = "Found communities", 
-            content = { @Content(mediaType = "application/json", 
-            schema = @Schema(implementation = Community.class)) }),
-        @ApiResponse(responseCode = "404", description = "The communities were not found", 
-            content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal server error", 
-            content = @Content)
-        })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found communities", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Community.class)) }),
+            @ApiResponse(responseCode = "404", description = "The communities were not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @GetMapping("/view/all")
     public ResponseEntity<List<Community>> getAllCommunities() {
         try {
@@ -73,7 +64,7 @@ public class CommunityController {
 
             communityRepository.findAll().forEach(CommunityList::add);
 
-            if(CommunityList.isEmpty()) {
+            if (CommunityList.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
 
@@ -84,81 +75,66 @@ public class CommunityController {
     }
 
     @Operation(summary = "Get a community by its id")
-    @ApiResponses(value = { 
-        @ApiResponse(responseCode = "200", description = "Found the community", 
-            content = { @Content(mediaType = "application/json", 
-            schema = @Schema(implementation = Community.class)) }),
-        @ApiResponse(responseCode = "400", description = "Invalid id supplied", 
-            content = @Content), 
-        @ApiResponse(responseCode = "404", description = "The community was not found", 
-            content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal server error", 
-            content = @Content)
-        })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the community", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Community.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "The community was not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @GetMapping("/view/{id}")
     public ResponseEntity<Community> getCommunityById(@PathVariable("id") long id) {
         Optional<Community> CommunityData = communityRepository.findById(id);
 
-        if(CommunityData.isPresent()) {
+        if (CommunityData.isPresent()) {
             return new ResponseEntity<>(CommunityData.get(), HttpStatus.OK);
-        }
-        else {
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @Operation(summary = "Edits a community by its id")
-    @ApiResponses(value = { 
-        @ApiResponse(responseCode = "200", description = "Community updated", 
-            content = { @Content(mediaType = "application/json", 
-            schema = @Schema(implementation = Community.class)) }),
-        @ApiResponse(responseCode = "400", description = "Invalid id supplied", 
-            content = @Content), 
-        @ApiResponse(responseCode = "404", description = "The community was not found", 
-            content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal server error", 
-            content = @Content)
-        })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Community updated", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Community.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "The community was not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @PutMapping("/edit/{id}")
-    public ResponseEntity<Community> updateCommunity(@PathVariable("id") long id, @RequestBody Community updateCommunity) {
+    public ResponseEntity<Community> updateCommunity(@PathVariable("id") long id,
+            @RequestBody Community updateCommunity) {
         Optional<Community> communityData = communityRepository.findById(id);
 
-        if(communityData.isPresent()) {
+        if (communityData.isPresent()) {
             Community modifiedCommunity = communityData.get();
             modifiedCommunity = new Community(updateCommunity);
             return new ResponseEntity<>(communityRepository.save(modifiedCommunity), HttpStatus.OK);
-        }
-        else {
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @Operation(summary = "Deletes a community by its id")
-    @ApiResponses(value = { 
-        @ApiResponse(responseCode = "200", description = "Community deleted", 
-            content = { @Content(mediaType = "application/json", 
-            schema = @Schema(implementation = Community.class)) }),
-        @ApiResponse(responseCode = "400", description = "Invalid id supplied", 
-            content = @Content), 
-        @ApiResponse(responseCode = "404", description = "The community was not found", 
-            content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal server error", 
-            content = @Content)
-        })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Community deleted", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = Community.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "The community was not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Community> deleteCommunity(@PathVariable("id") long id) {
         Optional<Community> communityData = communityRepository.findById(id);
-        if(communityData.isPresent()) {
+        if (communityData.isPresent()) {
             try {
                 Community deletedCommunity = communityData.get();
                 communityRepository.deleteById(id);
                 return new ResponseEntity<>(deletedCommunity, HttpStatus.OK);
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
                 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
-        }
-        else {
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
